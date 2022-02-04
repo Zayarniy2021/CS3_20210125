37c70a3e40d7191658f89f670fd6881c2e60ed90
Иванов Иван <zaazaa@yandex.ru>
2022-02-04 22:07:06 +0300

    Добавлены тесты

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Utilites.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static int TestsCount = 0;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            System.IO.File.AppendAllText("tests.log", $"Test {++TestsCount} запушен в {DateTime.Now}\r\n");
+        }
+
+        [TestMethod]
+        public void TestSum1()
+        {
+            int sum;
+            var serviceRes = Utilites.Models.Service.Sum(2,2);
+            int correctRes = 4;
+            Assert.AreEqual(correctRes, serviceRes);
+
+        }
+
+        [ClassInitialize]
+        public static void InitClass(TestContext context)
+        {
+            //будет запускаться перед каждым тестом
+            System.IO.File.AppendAllText("tests.log", $"Test {context.TestName} starts in {DateTime.Now}\r\n");
+        }
+    }
+}
